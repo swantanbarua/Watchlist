11499aab97131cd18c22ef0dfc38b173452f9627
Swantan Barua <work.swantan@gmail.com>
2025-09-21 23:51:50 +0530

    Add Movie data to model container

--- Watchlist/Model/Movie.swift
+++ Watchlist/Model/Movie.swift
@@ -22,6 +22,90 @@ class Movie {
             configurations: ModelConfiguration(isStoredInMemoryOnly: true)
         )
         
+        container.mainContext.insert(
+            Movie(
+                title: "John Wick",
+                genre: Genre(rawValue: 1)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "Groundhog Day",
+                genre: Genre(rawValue: 2)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "",
+                genre: Genre(rawValue: 3)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "",
+                genre: Genre(rawValue: 4)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "The Revenant",
+                genre: Genre(rawValue: 5)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "Lord of The Rings",
+                genre: Genre(rawValue: 6)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "Toy Story",
+                genre: Genre(rawValue: 7)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "",
+                genre: Genre(rawValue: 8)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "Blade Runner",
+                genre: Genre(rawValue: 9)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "Dirty Dancing",
+                genre: Genre(rawValue: 10)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "",
+                genre: Genre(rawValue: 11)!
+            )
+        )
+        
+        container.mainContext.insert(
+            Movie(
+                title: "Magnificient Seven",
+                genre: Genre(rawValue: 12)!
+            )
+        )
+        
         return container
     }
     
