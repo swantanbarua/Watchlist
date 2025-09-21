47560080f5d4d330e7a0887d3b5a8d59e3f977b1
Swantan Barua <work.swantan@gmail.com>
2025-09-21 23:29:39 +0530

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Swantan Barua on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchlistTests/WatchlistTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistTests.swift
+//  WatchlistTests
+//
+//  Created by Swantan Barua on 21/09/25.
+//
+
+import Testing
+@testable import Watchlist
+
+struct WatchlistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
