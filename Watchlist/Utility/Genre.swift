7182b76f80f8facafed25eb1697a788642664049
Swantan Barua <work.swantan@gmail.com>
2025-09-21 23:40:33 +0530

    Cover Genre name cases

--- Watchlist/Utility/Genre.swift
+++ Watchlist/Utility/Genre.swift
@@ -28,3 +28,48 @@ enum Genre: Int, Codable, CaseIterable, Identifiable {
     case thriller = 11
     case western = 12
 }
+
+extension Genre {
+    
+    // MARK: - PROPERTIES
+    var name: String {
+        switch self {
+            
+        case .action:
+            "Action"
+            
+        case .comedy:
+            "Comedy"
+            
+        case .crime:
+            "Crime"
+            
+        case .documentary:
+            "Documentary"
+            
+        case .drama:
+            "Drama"
+            
+        case .fantasy:
+            "Fantasy"
+            
+        case .kids:
+            "Kids"
+            
+        case .musical:
+            "Musical"
+            
+        case .romance:
+            "Romance"
+            
+        case .scifi:
+            "Sci-Fi"
+            
+        case .thriller:
+            "Thriller"
+            
+        case .western:
+            "Western"
+        }
+    }
+}
