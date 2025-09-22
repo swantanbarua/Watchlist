809e6079024ae07f0b489b3fc3a194d84038e471
Swantan Barua <work.swantan@gmail.com>
2025-09-22 23:42:46 +0530

    Add ContentUnavailable View

--- Watchlist/EmptyListView.swift
+++ Watchlist/EmptyListView.swift
@@ -11,32 +11,46 @@ struct EmptyListView: View {
     
     // MARK: - BODY
     var body: some View {
-        Circle()
-            .fill(.blue.gradient)
-            .stroke(
-                .blue.gradient.opacity(0.15),
-                lineWidth: 30
-            )
-            .stroke(
-                .blue.gradient.opacity(0.15),
-                lineWidth: 60
-            )
-            .stroke(
-                .blue.gradient.opacity(0.15),
-                lineWidth: 90
-            )
-            .frame(width: 180)
-            .overlay {
-                Image(systemName: "movieclapper")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100)
-                    .foregroundStyle(.white)
-                    .padding(
-                        .top,
-                        -6
-                    )
+        ContentUnavailableView {
+            Circle()
+                .fill(.blue.gradient)
+                .stroke(
+                    .blue.gradient.opacity(0.15),
+                    lineWidth: 30
+                )
+                .stroke(
+                    .blue.gradient.opacity(0.15),
+                    lineWidth: 60
+                )
+                .stroke(
+                    .blue.gradient.opacity(0.15),
+                    lineWidth: 90
+                )
+                .frame(width: 180)
+                .overlay {
+                    Image(systemName: "movieclapper")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100)
+                        .foregroundStyle(.white)
+                        .padding(
+                            .top,
+                            -6
+                        )
+                }
+        } description: {
+            GroupBox {
+                PageTabView(
+                    icon: "1.circle",
+                    description: "Add some movies to the watchlist if you plan to watch them later"
+                )
+                
+                PageTabView(
+                    icon: "2.circle",
+                    description: "Later on you can also choose a random film to watch"
+                )
             }
+        }
     }
 }
 
